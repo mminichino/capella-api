8ac0226a3b7521d30fac9b00008aa7a1198aacbd
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-08-22 18:27:41 -0500

    Version 1.0.2

--- tests/test_4.py
+++ tests/test_4.py
@@ -8,7 +8,11 @@ from libcapella.config import CapellaConfig
 from libcapella.organization import CapellaOrganization
 from libcapella.project import CapellaProject
 from libcapella.database import CapellaDatabase
+from libcapella.allowed_cidr import CapellaAllowedCIDR
+from libcapella.credentials import CapellaDatabaseCredentials
 from libcapella.logic.database import CapellaDatabaseBuilder
+from libcapella.logic.allowed_cidr import AllowedCIDRBuilder
+from libcapella.logic.credentials import DatabaseCredentialsBuilder
 from tests.common import get_account_email
 
 warnings.filterwarnings("ignore")
@@ -24,6 +28,9 @@ class TestDatabase(unittest.TestCase):
     def setUpClass(cls):
         cls.database_name = "pytest-cluster"
         cls.project_name = "pytest-project"
+        cls.cidr = "0.0.0.0/0"
+        cls.username = "developer"
+        cls.password = "P@ssw0rd!"
         cls.email = get_account_email()
         if not cls.email:
             raise RuntimeError('account email not set')
@@ -33,24 +40,39 @@ class TestDatabase(unittest.TestCase):
         if not cls.project.id:
             raise RuntimeError('project does not exist')
         cls.database = CapellaDatabase(cls.project, cls.database_name)
+        cls.allowed_cidr = None
+        cls.database_credential = None
 
     @classmethod
     def tearDownClass(cls):
         pass
 
     def test_1(self):
-        if self.database.id:
-            logger.debug(f"Database {self.database_name} already exists")
-            return
+        if not self.database.id:
+            builder = CapellaDatabaseBuilder("aws")
+            builder = builder.name(self.database_name)
+            builder = builder.description("Pytest created cluster")
+            builder = builder.region("us-east-2")
+            builder = builder.service_group("4x16", 3, 256)
+            config = builder.build()
+            self.database.create(config)
+            assert self.database.id is not None
 
-        builder = CapellaDatabaseBuilder("aws")
-        builder = builder.name(self.database_name)
-        builder = builder.description("Pytest created cluster")
-        builder = builder.region("us-east-2")
-        builder = builder.service_group("4x16", 3, 256)
-        config = builder.build()
-        self.database.create(config)
-        assert self.database.id is not None
+        self.allowed_cidr = CapellaAllowedCIDR(self.database, self.cidr)
+        if not self.allowed_cidr.id:
+            builder = AllowedCIDRBuilder()
+            builder.cidr(self.cidr)
+            config = builder.build()
+            self.allowed_cidr.create(config)
+            assert self.allowed_cidr.id is not None
+
+        self.database_credential = CapellaDatabaseCredentials(self.database, self.username)
+        if not self.database_credential.id:
+            builder = DatabaseCredentialsBuilder(self.username, self.password)
+            builder.data_read_write()
+            config = builder.build()
+            self.database_credential.create(config)
+            assert self.database_credential.id is not None
 
     def test_2(self):
         database_id = self.database.id
@@ -62,8 +84,8 @@ class TestDatabase(unittest.TestCase):
     def test_3(self):
         self.database.wait("deploying")
 
-    def test_4(self):
-        self.database.delete()
-
-    def test_5(self):
-        self.database.wait("destroying")
+    # def test_4(self):
+    #     self.database.delete()
+    #
+    # def test_5(self):
+    #     self.database.wait("destroying")
